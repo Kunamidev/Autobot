b7aa3794dcc00268ef07a74203846be316c56ed1
Rona Dev <kunnami661@gmail.com>
2024-06-26 09:39:26 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -5,10 +5,10 @@ module.exports.config = {
     version: '1.0.0',
     role: 0,
     hasPrefix: false,
-    aliases: ['ai'],
+    aliases: ['ai', 'bot'],
     description: 'AI Command',
     usage: 'ai [query]',
-    credits: 'developer',
+    credits: 'churchill',
     cooldown: 3,
 };
 
@@ -16,18 +16,20 @@ module.exports.run = async function({ api, event, args }) {
     const query = args.join(' ');
 
     if (!query) {
-        api.sendMessage('Please provide a question ex: ai what love', event.threadID, event.messageID);
+        api.sendMessage('Please provide a question example:\nai what is love?', event.threadID, event.messageID);
         return;
     }
 
-    const initialMessage = await api.sendMessage('Searching please wait.....', event.threadID, event.messageID);
+    api.sendMessage('Searching for an answer please wait....', event.threadID, event.messageID);
 
     try {
+        
         const aiResponse = await axios.get('https://markdevs-api.onrender.com/gpt4', {
             params: { prompt: query, uid: event.senderID }
         });
         const aiData = aiResponse.data.gpt4;
 
+        
         api.getUserInfo(event.senderID, (err, result) => {
             if (err) {
                 console.error('Error fetching user info:', err);
@@ -38,22 +40,12 @@ module.exports.run = async function({ api, event, args }) {
             const userName = result[event.senderID].name;
 
             // Send the combined response
-            const finalResponse = `${aiData}\n\nQuestion asked by: ${userName}`;
-            api.sendMessage(finalResponse, event.threadID, (err, messageInfo) => {
-                if (err) {
-                    console.error('Error sending message:', err);
-                } else {
-                    // Edit the initial message to the final response
-                    api.editMessage(finalResponse, messageInfo.messageID, (err) => {
-                        if (err) {
-                            console.error('Error editing message:', err);
-                        }
-                    });
-                }
-            });
+            const finalResponse = `**${aiData}**\n\nQuestion asked by: ${userName}`;
+            api.sendMessage(finalResponse, event.threadID, event.messageID);
         });
     } catch (error) {
         console.error('Error:', error);
         api.sendMessage('An error occurred while fetching the response.', event.threadID, event.messageID);
     }
 };
+                                         
