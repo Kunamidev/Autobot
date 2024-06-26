2f03878a74b386298f5942946209d0597210321d
Rona Dev <kunnami661@gmail.com>
2024-06-26 21:22:45 +0800

    Update gpt.js

--- script/gpt.js
+++ script/gpt.js
@@ -1,8 +1,28 @@
 const { get } = require('axios');
 
+function formatFont(text) {
+  const fontMapping = {
+    a: "𝚊", b: "𝚋", c: "𝚌", d: "𝚍", e: "𝚎", f: "𝚏", g: "𝚐", h: "𝚑", i: "𝚒", j: "𝚓", k: "𝚔", l: "𝚕", m: "𝚖",
+    n: "𝚗", o: "𝚘", p: "𝚙", q: "𝚚", r: "𝚛", s: "𝚜", t: "𝚝", u: "𝚞", v: "𝚟", w: "𝚠", x: "𝚡", y: "𝚢", z: "𝚣",
+    A: "𝙰", B: "𝙱", C: "𝙲", D: "𝙳", E: "𝙴", F: "𝙵", G: "𝙶", H: "𝙷", I: "𝙸", J: "𝙹", K: "𝙺", L: "𝙻", M: "𝙼",
+    N: "𝙽", O: "𝙾", P: "𝙿", Q: "𝚀", R: "𝚁", S: "𝚂", T: "𝚃", U: "𝚄", V: "𝚅", W: "𝚆", X: "𝚇", Y: "𝚈", Z: "𝚉"
+  };
+
+  let formattedText = "";
+  for (const char of text) {
+    if (char in fontMapping) {
+      formattedText += fontMapping[char];
+    } else {
+      formattedText += char;
+    }
+  }
+
+  return formattedText;
+}
+
 module.exports.config = {
   name: 'gpt',
-  credits: "cliff",
+  credits: "heru | rona",
   version: '1.0.0',
   role: 0,
   aliases: ["Gpt"],
@@ -13,18 +33,16 @@ module.exports.config = {
 
 module.exports.run = async function ({ api, event, args }) {
   const question = args.join(' ');
-  const { messageID } = event;
-
   function sendMessage(msg) {
-    api.sendMessage(msg, event.threadID, messageID);
+    api.sendMessage(msg, event.threadID, event.messageID);
   }
 
   const url = "https://hercai.onrender.com/v3/hercai";
 
-  if (!question) return sendMessage("Please provide a question.");
+  if (!question) return sendMessage("(❓) 𝙿𝚕𝚎𝚊𝚜𝚎 𝚙𝚛𝚘𝚟𝚒𝚍𝚎 𝚊 𝚚𝚞𝚎𝚜𝚝𝚒𝚘𝚗 𝚏𝚒𝚛𝚜𝚝.");
 
   // Send initial message and set "⌛" reaction
-  api.sendMessage('Searching, please wait...', event.threadID, async (err, messageInfo) => {
+  api.sendMessage('(⌛) 𝚂𝚎𝚊𝚛𝚌𝚑𝚒𝚗𝚐 𝚙𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝....', event.threadID, async (err, messageInfo) => {
     if (err) {
       console.error('Error sending initial message:', err);
       return;
@@ -39,11 +57,10 @@ module.exports.run = async function ({ api, event, args }) {
 
     try {
       const response = await get(`${url}?question=${encodeURIComponent(question)}`);
-      sendMessage(response.data.reply);
+      const formattedResponse = formatFont(response.data.reply);
 
-      // Fetch data from axios and edit the message
-      const marky = await get(``); // Add the correct URL here
-      api.editMessage(`${response.data.reply}\n\n${marky.data}`, searchMessageID, (err) => {
+      // Edit the initial message with the final response
+      api.editMessage(formattedResponse, searchMessageID, (err) => {
         if (err) {
           console.error('Error editing message:', err);
         }
@@ -60,3 +77,4 @@ module.exports.run = async function ({ api, event, args }) {
     }
   });
 };
+        
