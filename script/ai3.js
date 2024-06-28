99f420aae8af58b3d5910d341500468c4887e6b6
Rona Dev <kunnami661@gmail.com>
2024-06-28 18:47:00 +0800

    Update ai3.js

--- script/ai3.js
+++ script/ai3.js
@@ -1,5 +1,5 @@
 const { get } = require('axios');
-const url = "https://markdevs-last-api-cvxr.onrender.com";
+const url = "https://markdevs-api.onrender.com";
 
 module.exports.config = {
     name: "ai3",
@@ -13,20 +13,22 @@ module.exports.config = {
 };
 
 module.exports.run = async function({ event, args, api }) {
-    const { messageID } = event;
-    let prompt = args.join(' '), id = event.senderID;
+    const { threadID, messageID, senderID } = event;
+    let prompt = args.join(' ');
+
     if (!prompt) {
-        return api.sendMessage("Please provide a question first.", event.threadID, messageID);
+        return api.sendMessage("Please provide a question first.", threadID, messageID);
     }
 
-    const pending = await api.sendMessage("⏳ | Please bear with me while I ponder your request...", event.threadID, messageID);
+    const pending = await api.sendMessage("⏳ | Please bear with me while I ponder your request...", threadID, messageID);
 
     try {
-        const res = await get(url + "/gpt4?prompt=" + encodeURIComponent(prompt) + "&uid=" + id);
+        const res = await get(`${url}/gpt4?prompt=${encodeURIComponent(prompt)}&uid=${senderID}`);
         const answer = res.data.gpt4;
 
-        return api.editMessage(`🌺 𝐑𝐎𝐍𝐀 𝐀𝐈 𝐯.1\n━━━━━━━━━━━━━━━━━━\n𝗤𝘂𝗲𝘀𝘁𝗶𝗼𝗻: ${prompt}\n━━━━━━━━━━━━━━━━━━\n𝗔𝗻𝘀𝘄𝗲𝗿: ${answer}\n━━━━━━━━━━━━━━━━━━\n🌺 𝚁𝚘𝚗𝚊 𝚊𝚞𝚝𝚘𝚋𝚘𝚝 𝟎.𝟎.𝟏`, pending.messageID);
+        return api.sendMessage(`🌺 𝐑𝐎𝐍𝐀 𝐀𝐈 𝐯.1\n━━━━━━━━━━━━━━━━━━\n𝗤𝘂𝗲𝘀𝘁𝗶𝗼𝗻: ${prompt}\n━━━━━━━━━━━━━━━━━━\n𝗔𝗻𝘀𝘄𝗲𝗿: ${answer}\n━━━━━━━━━━━━━━━━━━\n🌺 𝚁𝚘𝚗𝚊 𝚊𝚞𝚝𝚘𝚋𝚘𝚝 𝟎.𝟎.𝟏`, threadID, pending.messageID);
     } catch (e) {
-        return api.sendMessage(e.message, event.threadID, messageID);
+        return api.sendMessage(e.message, threadID, messageID);
     }
 };
+            
