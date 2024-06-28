5f7d4fff51838a1b2c842c0c1a8b5f7a4dd99163
Rona Dev <kunnami661@gmail.com>
2024-06-28 14:16:20 +0800

    Update research.js

--- script/research.js
+++ script/research.js
@@ -1,5 +1,25 @@
 const axios = require('axios');
 
+function formatFont(text) {
+  const fontMapping = {
+    a: "𝚊", b: "𝚋", c: "𝚌", d: "𝚍", e: "𝚎", f: "𝚏", g: "𝚐", h: "𝚑", i: "𝚒", j: "𝚓", k: "𝚔", l: "𝚕", m: "𝚖",
+    n: "𝚗", o: "𝚘", p: "𝚙", q: "𝚚", r: "𝚛", s: "𝚜", t: "𝚝", u: "𝚞", v: "𝚟", w: "𝚠", x: "𝚡", y: "𝚢", z: "𝚣",
+    A: "𝙰", B: "𝙱", C: "𝙲", D: "𝙳", E: "𝙴", F: "𝙵", G: "𝙶", H: "𝙷", I: "𝙸", J: "𝙹", K: "𝙺", L: "𝙻", M: "𝙼",
+    N: "𝙽", O: "𝙾", P: "𝙿", Q: "𝚀", R: "𝚁", S: "𝚂", T: "𝚃", U: "𝚄", V: "𝚅", W: "𝚆", X: "𝚇", Y: "𝚈", Z: "𝚉"
+  };
+
+  let formattedText = "";
+  for (const char of text) {
+    if (char in fontMapping) {
+      formattedText += fontMapping[char];
+    } else {
+      formattedText += char;
+    }
+  }
+
+  return formattedText;
+}
+
 module.exports.config = {
   name: "research",
   version: "1.0.0",
@@ -17,7 +37,13 @@ module.exports.run = async function ({ api, event, args }) {
   const query = args.join(' ');
 
   if (!query) {
-    api.sendMessage('Please provide a search query for Arxiv.', threadID, messageID);
+    api.sendMessage('Please provide a search query for Arxiv.', threadID, (err, messageInfo) => {
+      if (!err) {
+        setTimeout(() => {
+          api.unsendMessage(messageInfo.messageID);
+        }, 6000);
+      }
+    });
     return;
   }
 
@@ -26,17 +52,36 @@ module.exports.run = async function ({ api, event, args }) {
     const data = response.data;
 
     if (!data.title) {
-      api.sendMessage('No research articles found on Arxiv for the given query.', threadID, messageID);
+      api.sendMessage('No research articles found on Arxiv for the given query.', threadID, (err, messageInfo) => {
+        if (!err) {
+          setTimeout(() => {
+            api.unsendMessage(messageInfo.messageID);
+          }, 6000);
+        }
+      });
       return;
     }
 
     const { title, authors, published, summary } = data;
 
-    const responseMessage = `📚 Arxiv Research Article\n\n📝 Title: ${title}\n\n👥 Authors: ${authors.join(', ')}\n\n🗓️ Published Date: ${published}\n\n📖 Summary: ${summary}`;
+    const responseMessage = formatFont(`📚 Arxiv Research Article\n\n📝 Title: ${title}\n\n👥 Authors: ${authors.join(', ')}\n\n🗓️ Published Date: ${published}\n\n📖 Summary: ${summary}`);
 
-    api.sendMessage(responseMessage, threadID, messageID);
+    api.sendMessage(responseMessage, threadID, (err, messageInfo) => {
+      if (!err) {
+        setTimeout(() => {
+          api.unsendMessage(messageInfo.messageID);
+        }, 6000);
+      }
+    });
   } catch (error) {
     console.error(error);
-    api.sendMessage('An error occurred while fetching Arxiv data.', threadID, messageID);
+    api.sendMessage('An error occurred while fetching Arxiv data.', threadID, (err, messageInfo) => {
+      if (!err) {
+        setTimeout(() => {
+          api.unsendMessage(messageInfo.messageID);
+        }, 6000);
+      }
+    });
   }
 };
+  
