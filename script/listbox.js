dff271e66d40084a67af332ba8209622d26ed468
Rona Dev <kunnami661@gmail.com>
2024-06-29 10:39:09 +0800

    Update listbox.js

--- script/listbox.js
+++ script/listbox.js
@@ -1,23 +1,34 @@
 module.exports.config = {
-		name: "listbox",
-		version: "1.0.0",
-		credits: "Him",
-		role: 0,
-		description: "Lấy tên và id các nhóm chứa bot",
-		hasPrefix: false,
-	  aliases: ["allbox"],
-		usage: "allbox",
-		cooldown: 5
+	name: "listbox",
+	version: "1.0.0",
+	credits: "Him",
+	role: 0,
+	description: "Lấy tên và id các nhóm chứa bot",
+	hasPrefix: false,
+	aliases: ["allbox"],
+	usage: "allbox",
+	cooldown: 5
 };
 
 module.exports.run = async function ({ api, event }) {
-		var num = 0, box = "";
-		api.getThreadList(100, null, ["INBOX"], (err, list) => {
-				list.forEach(info => {
-						if (info.isGroup && info.isSubscribed) {
-								box += `${num+=1}. ${info.name} - ${info.threadID}\n`;
-						}			
-				});
-				api.sendMessage(box, event.threadID, event.messageID);
+	let num = 0;
+	let box = "";
+	api.getThreadList(100, null, ["INBOX"], (err, list) => {
+		if (err) return console.error(err);
+		
+		list.forEach(info => {
+			if (info.isGroup && info.isSubscribed) {
+				box += `${num += 1}. ${info.name} - ${info.threadID}\n`;
+			}
 		});
+		
+		api.sendMessage(box, event.threadID, (err, messageInfo) => {
+			if (err) return console.error(err);
+
+			// Auto unsend the message after 6 seconds
+			setTimeout(() => {
+				api.deleteMessage(messageInfo.messageID);
+			}, 6000);
+		}, event.messageID);
+	});
 };
