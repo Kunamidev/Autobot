344f46c651d068ceb40dff4e9f68b63cf8c5865c
Rona Dev <kunnami661@gmail.com>
2024-06-29 10:44:02 +0800

    Update uid.js

--- script/uid.js
+++ script/uid.js
@@ -1,26 +1,47 @@
 module.exports.config = {
-	name: "uid",
-	role: 0,
-	credits: "Mirai Team",
-	description: "Get the user's Facebook UID.",
-	hasPrefix: false,
-	usages: "{p}uid {p}uid @mention",
-	cooldown: 5,
-	aliases: ["id","ui"]
+  name: "uid",
+  role: 0,
+  credits: "Mirai Team",
+  description: "Get the user's Facebook UID.",
+  hasPrefix: false,
+  usages: "{p}uid {p}uid @mention",
+  cooldown: 5,
+  aliases: ["id", "ui"]
 };
 
 module.exports.run = async function({ api, event }) {
-	if (Object.keys(event.mentions).length === 0) {
-		if (event.messageReply) {
-			const senderID = event.messageReply.senderID;
-			return api.sendMessage(senderID, event.threadID);
-		} else {
-			return api.sendMessage(`${event.senderID}`, event.threadID, event.messageID);
-		}
-	} else {
-		for (const mentionID in event.mentions) {
-			const mentionName = event.mentions[mentionID];
-			api.sendMessage(`${mentionName.replace('@', '')}: ${mentionID}`, event.threadID);
-		}
-	}
+  if (Object.keys(event.mentions).length === 0) {
+    if (event.messageReply) {
+      const senderID = event.messageReply.senderID;
+      api.sendMessage(senderID, event.threadID, (err, messageInfo) => {
+        if (err) return console.error(err);
+
+        // Auto unsend the message after 6 seconds
+        setTimeout(() => {
+          api.deleteMessage(messageInfo.messageID);
+        }, 6000);
+      });
+    } else {
+      api.sendMessage(`${event.senderID}`, event.threadID, (err, messageInfo) => {
+        if (err) return console.error(err);
+
+        // Auto unsend the message after 6 seconds
+        setTimeout(() => {
+          api.deleteMessage(messageInfo.messageID);
+        }, 6000);
+      }, event.messageID);
+    }
+  } else {
+    for (const mentionID in event.mentions) {
+      const mentionName = event.mentions[mentionID];
+      api.sendMessage(`${mentionName.replace('@', '')}: ${mentionID}`, event.threadID, (err, messageInfo) => {
+        if (err) return console.error(err);
+
+        // Auto unsend the message after 6 seconds
+        setTimeout(() => {
+          api.deleteMessage(messageInfo.messageID);
+        }, 6000);
+      });
+    }
+  }
 };
